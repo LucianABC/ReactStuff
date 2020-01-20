2311c755b4e55d9854d3011d5ef2f4b3a28a53e1
LucianABC <louabecasis@gmail.com>
2020-01-20 12:58:21 -0300

    details

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,6 +9,8 @@ const Card = props => {
                 <h2 className="card-info-subtitle">Villains</h2>
                 <h1 className="card-info-title">{props.villainName}</h1>
                 <p className ="card-info-text">{props.villainInfo}</p>
+
+                <span className="card-info-more">+</span>
             </article>
         </section>
 
