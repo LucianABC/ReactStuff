1e3a12f52b22e45dcbb3c029270714c39dec5b78
LucianABC <louabecasis@gmail.com>
2020-01-20 12:41:18 -0300

    position card info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import PoisonIvyimg from './components/imgs/poisonivy.jpg';
+import Catwomanimg from './components/imgs/catwoman.jpg';
+import HarleyQuinnimg from './components/imgs/harlequinn.jpg';
+
 
 function App() {
+  const villains = [
+    {name: "Poison Ivy",
+      img:  PoisonIvyimg,
+      info: "infojasdjkjaskdas"
+    },
+    {
+      name: "Catwoman",
+      img: Catwomanimg,
+      info: "dkjaslkdjaklsdjlaksjdlaskjdakljdals"
+
+    },
+    {
+      name: "Harley Quinn",
+      img: HarleyQuinnimg,
+      info: "m,sdhkjshdkajsdhwiuhdm,asnd,ma"
+
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {villains.map(villain => {
+          return <Card cardBckgrnd = {villain.img} villainName = {villain.name} villainInfo = {villain.info}></Card>
+        })}
     </div>
+
+
+
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.css'
+const Card = props => {
+    return(
+        <section className="card" style = {{
+                backgroundImage: `url(${props.cardBckgrnd})` 
+        }}>
+            <article className="card-info">
+                <h2 className="card-info-subtitle">Villains</h2>
+                <h1 className="card-info-title">{props.villainName}</h1>
+                <p className ="card-info-text">{props.villainInfo}</p>
+            </article>
+        </section>
+
+    );
+}
+export default Card;
